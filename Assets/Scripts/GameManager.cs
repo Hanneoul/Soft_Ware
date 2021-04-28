2b18c0a49176abcc0cf82937d8723efaeabe5863
SanaHus <81694241+SanaHus@users.noreply.github.com>
2021-04-28 13:29:10 +0900

    Revert "DreamTruck Prototype"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,31 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [HideInInspector] public static GameManager instance = null; //Singleton
-
-
-    void Awake()
-    {
-        Debug.Log("GameManager:Awake() Start.");
-        Screen.SetResolution(Screen.width, (Screen.width * 16) / 9 , true);
-        Debug.Log("GameManager:Resolution Setting Done.");
-
-        if (instance == null)
-            instance = this;
-        else if (instance != this)
-            Destroy(gameObject);
-
-       DontDestroyOnLoad(gameObject);
-
-       Debug.Log("GameManager:Awake() Done.");
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameManager:Start() Start.");
         
-        Debug.Log("GameManager:Start() Done.");
     }
 
     // Update is called once per frame
